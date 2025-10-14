97ba0eaf81e6ef55c60b7332c3e905d9bdf189ac
Hrithik085 <hrithikpramod085@gmail.com>
2025-10-14 23:01:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 import os
 
-BOT_TOKEN = "8330702607:AAHQdFy27Kqyx9RvEhJ528-q_72uzv9v8Xw"
+BOT_TOKEN = "7502385267:AAFpiuoLOUx3XVoUwxy1McI15v3VKCYeBc0"
 
 # Database URL will be set by Render's Environment
 DATABASE_URL = os.getenv("DATABASE_URL")
