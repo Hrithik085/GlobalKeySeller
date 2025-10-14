933013e61084ac38f28a6451b3e402f58d377e11
hrithik-at-105410518218 <hrithik.pramod@elogosoft.in>
2025-10-14 20:33:49 +0530

    ebv

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 BOT_TOKEN = "8330702607:AAHQdFy27Kqyx9RvEhJ528-q_72uzv9v8Xw"
 
-# Database URL will be set by Render's Environment Variables
+# Database URL will be set by Render's Environment
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Price settings
